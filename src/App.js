c35fe2a69f8604b0d76a75241668ae117b5beac4
Jared Teng <jaredteng1602@gmail.com>
2023-11-27 18:47:23 +1100

    add clickable dropdown menu

--- src/App.js
+++ src/App.js
@@ -7,16 +7,63 @@ import ShopifyLogo from './Javascript30 Navbar Starter Files/Assets/Shopify Logo
 import NetflixLogo from './Javascript30 Navbar Starter Files/Assets/Netflix Logo.svg';
 import WalmartLogo from './Javascript30 Navbar Starter Files/Assets/Walmart Logo.svg';
 import SpotifyLogo from './Javascript30 Navbar Starter Files/Assets/Spotify Logo.svg';
+import SpenseIcon from './Javascript30 Navbar Starter Files/Assets/Spense_Icon.svg';
+import FiberIcon from './Javascript30 Navbar Starter Files/Assets/Fiber_Icon.svg';
+import GradieIcon from './Javascript30 Navbar Starter Files/Assets/Gradie_Icon.svg';
+import { useState } from 'react';
 function Header() {
+  const [dropClicked, setDropClicked] = useState(false);
+
+  function Dropdown() {
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    if (dropClicked) {
+      setDropClicked(false);
+      dropdownMenu.className = 'dropdown-menu active';
+    } else {
+      setDropClicked(true);
+      dropdownMenu.className = 'dropdown-menu open';
+    }
+    console.log('clicked');
+  }
   return (
     <header className="App-header">
       <nav>
-        <img src={Logo} alt='logo'/>
+        <img src={Logo} alt='logo' />
         <div className='nav-links'>
-          <a>Products</a>
-          <a>Challanges</a>
-          <a>Resources</a>
-          <a>Other Links</a>
+          <ul className='nav-ul'>
+            <li className='dropdown'>
+              <a onClick={Dropdown}>Products</a>
+              <ul id='dropdown-menu' className='dropdown-menu active'>
+                <li>
+                  <img src={SpenseIcon} alt='image'/>
+                  <div className='dropdown-link-text'>
+                    <p>Spense</p>
+                    <p>Spense is a landing page for writers. Great
+                    for practising absolute positioning and flex layouts</p>
+                  </div>
+                </li>
+                <li>
+                  <img src={FiberIcon} alt='image'/>
+                  <div className='dropdown-link-text'>
+                    <p>Fiber Landing Page</p>
+                    <p>An online portfolio generator. Great to practice
+                    flex/grid layouts, and absolute positioning.</p>
+                  </div>
+                </li>
+                <li>
+                  <img src={GradieIcon} alt='image'/>
+                  <div className='dropdown-link-text'>
+                    <p>Gradie Sign Up Page</p>
+                    <p>Gradie is a simple sign up page, great to
+                    practice centering layouts.</p>
+                  </div>
+                </li>
+              </ul>
+            </li>
+            <li><a>Challanges</a></li>
+            <li><a>Resources</a></li>
+            <li><a>Other Links</a></li>
+          </ul>
         </div>
         <div className='login'>
           <a>Sign In</a>
@@ -36,11 +83,11 @@ function Content() {
         <a className='unique'>Try for free</a>
         <div>
           <p>5.0 Rating based on reviews from: </p>
-          <img src={CapterraLogo} alt='CapterraLogo'/>
-          <img src={AlternateLogo} alt='AlternateLogo'/>
+          <img src={CapterraLogo} alt='CapterraLogo' />
+          <img src={AlternateLogo} alt='AlternateLogo' />
         </div>
       </div>
-      <img src={HeroImage} alt='image'/>
+      <img src={HeroImage} alt='image' />
     </div>
   );
 }
@@ -50,10 +97,10 @@ function ContentEnd() {
     <div className='content-end'>
       <p>Trusted by 3+ million people at companies like</p>
       <div className='company-logos'>
-        <img src={NetflixLogo} alt='logo'/>
-        <img src={ShopifyLogo} alt='logo'/>
-        <img src={SpotifyLogo} alt='logo'/>
-        <img src={WalmartLogo} alt='logo'/>
+        <img src={NetflixLogo} alt='logo' />
+        <img src={ShopifyLogo} alt='logo' />
+        <img src={SpotifyLogo} alt='logo' />
+        <img src={WalmartLogo} alt='logo' />
       </div>
     </div>
   );
@@ -61,9 +108,9 @@ function ContentEnd() {
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Content/>
-      <ContentEnd/>
+      <Header />
+      <Content />
+      <ContentEnd />
     </div>
   );
 }
